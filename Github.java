2f551b1978f6564d3420d6131f212b8ff4b868f4
vxp3503 <55551399+vxp3503@users.noreply.github.com>
2019-10-01 23:04:52 +0530

    get and print name in java

---
+++ Github.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public Main{
+static Scanner in = new Scanner(System.in);
+public static void main(String args[]){
+String name;
+System.out.println("Please enter your name:");
+name=in.nextLine();
+System.out.println(name);
+}
+}
+

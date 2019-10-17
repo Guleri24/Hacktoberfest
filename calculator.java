64d8b36eacf0b33281aca1676844a14894d71def
BHUKYA VAMSHI RATHOD <40638238+bhukyavamshirathod@users.noreply.github.com>
2019-10-17 12:18:59 +0530

    Update calculator.java
    Updates....

--- calculator.java
+++ calculator.java
@@ -5,5 +5,7 @@ System.out.println("sum of "+num1+" and "+num2" = "+(num1+num2);
 System.out.println("sub of "+num1+" and "+num2" = "+(num1-num2);
 System.out.println("multiply of "+num1+" and "+num2" = "+(num1*num2);
 System.out.println("divide of "+num1+" and "+num2" = "+(num1/num2);
+
 }
+                   return 0;
 }
